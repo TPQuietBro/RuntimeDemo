8f77b84c8321fc91fbd5400adac3d5e31802c744
唐鹏 <tangpeng@icarbonx.com>
2018-03-13 18:58:00 +0800

    first commit

---
+++ RuntimeDemo/Animal.h
@@ -0,0 +1,13 @@
+//
+//  Animal.h
+//  RuntimeDemo
+//
+//  Created by tangpeng on 2018/3/13.
+//Copyright © 2018年 ICX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@end
---
+++ RuntimeDemo/Humen.m
@@ -0,0 +1,25 @@
+//
+//  Humen.m
+//  RuntimeDemo
+//
+//  Created by tangpeng on 2018/3/13.
+//Copyright © 2018年 ICX. All rights reserved.
+//
+
+#import "Humen.h"
+
+@implementation Humen
+#pragma mark - init methods
+
+#pragma mark - system delegate
+
+#pragma mark - custom delegate
+
+#pragma mark - api methods
+
+#pragma mark - event response
+
+#pragma mark - private
+
+#pragma mark - getter / setter
+@end
